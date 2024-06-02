e26e17eafce140542e75041308aa4db115e0d95b
Fabio <fabinho.j31.santos@gmail.com>
2024-06-02 19:07:07 -0300

    Adicionada função para listagem de usuário pelo ID e alteração do usuário pelo ID

--- src/main/java/br/com/fabiodossantos/todolist/user/UserController.java
+++ src/main/java/br/com/fabiodossantos/todolist/user/UserController.java
@@ -3,15 +3,20 @@ package br.com.fabiodossantos.todolist.user;
 import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.fabiodossantos.todolist.utils.Utils;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/users")
@@ -20,7 +25,7 @@ public class UserController {
     @Autowired
     private IUserRepository userRepository;
 
-    @PostMapping("/")    
+    @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
         // Verifica existência do usuário (busca pelo username)
         var user = this.userRepository.findByUsername(userModel.getUsername());
@@ -33,5 +38,29 @@ public class UserController {
         // Cria o usuário na base de dados e retorna como resposta da requisição
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity listById(@RequestBody UserModel userModel, @PathVariable UUID id) {
+        // Verifica existência do usuário (busca pelo id)
+        var user = this.userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não encontrado!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody UserModel userModel, @PathVariable UUID id) {
+        // Verifica existência do usuário (busca pelo id)
+        var user = this.userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não encontrado!");
+        }
+       // Atualiza os campos que foram passados na requisição
+        Utils.copyNonNullProperties(userModel, user);
+        // Atualiza os dados da tarefa
+        var userUpdated = this.userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.OK).body(userUpdated);
     }
 }
